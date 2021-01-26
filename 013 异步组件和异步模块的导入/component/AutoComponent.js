561787a78edee54601a1dac6c86a31957901d38b
xiaojian <oppor11520@gmail.com>
2021-01-26 18:55:15 +0800

    异步模块和组件的加载

---
+++ 013 异步组件和异步模块的导入/component/AutoComponent.js
@@ -0,0 +1,13 @@
+import { Component, Fragment } from 'react'
+
+class AutoComponent extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div>Fragment</div>
+            </Fragment>
+        )
+    }
+}
+
+export default AutoComponent
