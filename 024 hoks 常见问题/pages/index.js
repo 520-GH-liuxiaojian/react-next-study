39f0cfd6d3bfd88eb1aff927018612a6000bbbbb
xiaojian <oppor11520@gmail.com>
2021-01-30 18:38:39 +0800

    hooks useReducer 使用

--- 024 hoks 常见问题/pages/index.js
+++ 024 hoks 常见问题/pages/index.js
@@ -1,45 +1,45 @@
-class Counter extends Comment {
-    state = {
-        overflow: true,
+import { useReducer, useEffect } from 'react'
+
+class CountReducer {
+    countReducerType = {
+        'add': state => state + 1,
+        // 如果这里 state 是一个非常复杂的对象 则通过 Object.assign 处理才能返回
+        'minus': state => state - 1,
     }
 
-    static getDerivedStateFromProps(props, state) {
-        if(props.count > 10) {
-            return {
-                overflow: true
-            }
+    middleCountValue(state, action) {
+        try {
+            return this.countReducerType[action.type](state)
+        } catch (e) {
+            console.error(e.message)
+            window.alert(e.message)
         }
     }
 }
 
-function Counter(props) {
-    const [overflow, setOverflow] = useState(false)
-    if(props.count > 10) {
-        // 这里直接 setState 不会导致死循环嘛
-        // 会 逻辑出现问题就会导致死循环
-
-        // 直接 setState 会出现性能问题嘛
-        // 不会 setstate 是在组件的渲染之前就完成 不会有性能问题
-        setOverflow(true)
-    }
+function countReducer(state, action) {
+    return new CountReducer().middleCountValue(state, action)
 }
 
-function App() {
-    // 只调用一次
+function Home() {
+    // 如果 useReducer 写入默认复杂对象 这个时候 dispatchCount 进行更新是非常困难的
+    // dispatchCount 传入的是一个的新的对象 而不是一个老的对象 也不能直接在老的对象上进行修改
+    // 内部数据处理返回的时候 需要使用 Object.assign 处理 返回一个新的对象
+    // 只有对象改变了本身 我们才能认为这个对象是经过修改的 如果只是修改了老对象上的属性及其属性值我们不认为是老对象是通过修改的
+    // 如果将没有经过修改处理过后的对象传递给组件 组件一旦认为对象没有经过修改 则不会出发组件渲染行为
+    // 导致页面的处理 bug
+    const [count, dispatchCount] = useReducer(countReducer, 0)
+
     useEffect(() => {
-        // componentDidMount
-        return () => {
-            // componentWillUnmount
-        }
+        const interval = setInterval(() => {
+            dispatchCount({type: 'minus'})
+        }, 500)
+        return () => clearInterval(interval)
     }, [])
 
-    let renderCounter = useRef(0)
-    renderCounter.count ++
-
-    // 可以调用多次
-    useEffect(() => {
-        if(renderCounter > 1) {
-            // componentDidUpdate
-        }
-    })
+    return (
+        <button>click({count})</button>
+    )
 }
+
+export default Home
--- README.md
+++ README.md
@@ -993,6 +993,10 @@ export default Home
 
 
 
+**Hooks 就是让函数组件具有类组件的能力**
+
+
+
 ## hooks 组件优化点
 
 + 函数组件无 this 指向问题
@@ -1083,6 +1087,11 @@ const [count, setCount] = useState(() => {
 ```
 
 
+
+setCount 使用情况
+
++ setCount()  根据指定的值来更新 state
++ setCount(c => c +1 ) 根据当前 state 最新的值来更新 通过回调就可以规避值不更新的情况
 
 ## 使用 Effect hooks 
 
@@ -1578,4 +1587,53 @@ function Counter(props) {
 + 类实例成员变量如何映射到 Hooks
 + Hooks 中如何获取历史 props 和 state
 + 如何强制更新一个组件
+
+
+
+## useReducer 可以根据不同类型来更新 state 的值
+
+```javascript
+class CountReducer {
+    countReducerType = {
+        'add': state => state + 1,
+        // 如果这里 state 是一个非常复杂的对象 则通过 Object.assign 处理才能返回
+        'minus': state => state - 1,
+    }
+
+    middleCountValue(state, action) {
+        try {
+            return this.countReducerType[action.type](state)
+        } catch (e) {
+            console.error(e.message)
+            window.alert(e.message)
+        }
+    }
+}
+
+function countReducer(state, action) {
+  	// 这里的 state 就是最新状态
+  	// action 根据 action.type 的不同的执行不同的更新
+    return new CountReducer().middleCountValue(state, action)
+}
+
+    // 如果 useReducer 写入默认复杂对象 这个时候 dispatchCount 进行更新是非常困难的
+    // dispatchCount 传入的是一个的新的对象 而不是一个老的对象 也不能直接在老的对象上进行修改
+    // 内部数据处理返回的时候 需要使用 Object.assign 处理 返回一个新的对象
+    // 只有对象改变了本身 我们才能认为这个对象是经过修改的 如果只是修改了老对象上的属性及其属性值我们不认为是老对象是通过修改的
+    // 如果将没有经过修改处理过后的对象传递给组件 组件一旦认为对象没有经过修改 则不会出发组件渲染行为
+    // 导致页面的处理 bug
+
+		// useReducer 需要传入默认值
+    const [count, dispatchCount] = useReducer(countReducer, 0)
+
+dispatchCount({type: 'minus'})
+```
+
+在 dispatchCount 中就可以根据 类型来做的值的增加和修改 相较于useEffect 更加灵活
+
+
+
+
+
+
 
